547de72a9199516e93b836ad044cf10d3213edec
stephy <stephshiro35@gmail.com>
2024-06-05 12:28:05 +0300

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1, "Mr. Chocolate", 26, "M", "Grey", "Anti-Social", 1);
+INSERT INTO bears VALUES(2, "Rowdy", 32, "M", "Red", "Tough", 0);
+INSERT INTO bears VALUES(3, "Tabitha", 22, "F", "White", "Social", 1);
+INSERT INTO bears VALUES(4, "Sergent Brown", 12, "M", "Black", "Tough", 0);
+INSERT INTO bears VALUES(5, "Melissa", 28, "F", "Black", "Tough", 1);
+INSERT INTO bears VALUES(6, "Grinch", 19, "M", "White", "Funny", 1);
+INSERT INTO bears VALUES(7, "Wendy", 25, "F", "Black", "Optimistic", 1);
+INSERT INTO bears VALUES(8, NULL, 8, "F", "White", "jovial", 0);
